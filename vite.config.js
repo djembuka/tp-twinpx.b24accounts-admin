cc33aa64ff5448c11c707ecb603c782858dc3f3e
Tatiana <djembuka@gmail.com>
2026-03-24 19:02:57 +0300

    feat: change names of css js files

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,20 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         controls: resolve(__dirname, 'controls.html')
+      },
+      output: {
+        chunkFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'main')
+            return 'assets/script.js';
+        },
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.names?.[0] ?? '';
+
+          if (name.includes('main.css')) {
+            return 'assets/styles.css'
+          }
+          return 'assets/[name].[ext]'
+        }
       }
     }
   }
